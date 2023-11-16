3aa93d6923563eabe3a668ad2563fe8465591a5c
Hugo Antón Cuadrado <Hugoacacademico@gmail.com>
2023-11-16 11:16:11 +0100

    Base de datos creada

--- MS_HA/conecta.php
+++ MS_HA/conecta.php
@@ -9,7 +9,14 @@ $conexion = mysqli_connect($servername, $username, $password, $dbname);
 
 if (!$conexion) {
     die("Conexión fallida: " . mysqli_connect_error());
+} else {
+    echo "Conectado";
+    //Creamos la BD
+    $sql = "CREATE DATABASE biblioteca";
+    if (mysqli_query($conexion, $sql)) { // Lanzar BD contra el servidor
+        echo "Base de datos creada con éxito";
+    } else {
+        echo "Error al crear la base de datos";
+        mysqli_error($conexion); //Muestra el código de error
+    }
 }
-
-
-?>
