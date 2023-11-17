66e57e7eb580d38ca60fd7cacc9ba850e1304dd6
Hugo Antón Cuadrado <Hugoacacademico@gmail.com>
2023-11-17 11:40:49 +0100

    Creación de tablas correcto

--- MS_HA/crear_tablas.php
+++ MS_HA/crear_tablas.php
@@ -1,6 +1,5 @@
 <?php
 
-
 require_once("conecta.php");
 $conexion = conecta();
 $base_datos = 'biblioteca';
@@ -26,23 +25,29 @@ $sql_lectores = "CREATE TABLE IF NOT EXISTS lectores (
 )";
 
 $sql_prestamo = "CREATE TABLE IF NOT EXISTS prestamo (
-    id_lector INT,
-    id_libro INT,
-    FOREIGN KEY (id_lector) REFERENCES lectores(id),
-    FOREIGN KEY (id_libro) REFERENCES libros(id)
+    id_lector INT NOT NULL,
+    id_libro INT NOT NULL,
+    CONSTRAINT fk_lector FOREIGN KEY (id_lector) REFERENCES lectores(id),
+    CONSTRAINT fk_libro FOREIGN KEY (id_libro) REFERENCES libros(id)
 )";
-
 
 mysqli_query($conexion, $sql_libros);
 mysqli_query($conexion, $sql_lectores);
 mysqli_query($conexion, $sql_prestamo);
 
+$sql = "SELECT lector FROM lectores";
+$resultado = mysqli_query($conexion, $sql) or die("Error al comprobar los datos");
 
-mysqli_query($conexion, "INSERT INTO libros (nombre, autor, publicacion, ISBN, n_disponibles, n_totales) VALUES ('Harry Potter y el prisionero de Azkaban', 'JK Rowling', 1999, 1234567890123, 5, 10)");
-mysqli_query($conexion, "INSERT INTO libros (nombre, autor, publicacion, ISBN, n_disponibles, n_totales) VALUES ('Boulevard', 'Flor M. Salvador', 2020, 9789807909068, 7, 10)");
-mysqli_query($conexion, "INSERT INTO lectores (lector, DNI) VALUES ('Fernando Alonso Diaz', '12335678A')");
-mysqli_query($conexion, "INSERT INTO lectores (lector, DNI) VALUES ('Carlos Sainz Vazquez', '12345578N')");
-mysqli_query($conexion, "INSERT INTO prestamo (id_lector, id_libro) VALUES (1, 1)");
-mysqli_query($conexion, "INSERT INTO prestamo (id_lector, id_libro) VALUES (2, 1)");
+if(mysqli_num_rows($resultado) == 0){
+    $libro1 = "INSERT INTO libros (nombre, autor, publicacion, ISBN, n_disponibles, n_totales) VALUES ('Harry Potter y el prisionero de Azkaban', 'JK Rowling', 1999, 1234567890123, 5, 10)";
+    mysqli_query($conexion, $libro1);
+
+    mysqli_query($conexion, "INSERT INTO libros (nombre, autor, publicacion, ISBN, n_disponibles, n_totales) VALUES ('Boulevard', 'Flor M. Salvador', 2020, 9789807909068, 7, 10)");
+    mysqli_query($conexion, "INSERT INTO lectores (lector, DNI) VALUES ('Fernando Alonso Diaz', '12335678A')");
+    mysqli_query($conexion, "INSERT INTO lectores (lector, DNI) VALUES ('Carlos Sainz Vazquez', '12345578N')");
+    mysqli_query($conexion, "INSERT INTO prestamo (id_lector, id_libro) VALUES (1, 1)");
+    mysqli_query($conexion, "INSERT INTO prestamo (id_lector, id_libro) VALUES (2, 1)");
+
+}
 
 mysqli_close($conexion);
