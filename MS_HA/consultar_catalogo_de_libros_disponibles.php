9fbb2b7b4baacf7525d1b7e7546a6cb4ba074fbf
Miguel Sanz <118904961+Miguel-Sanz@users.noreply.github.com>
2023-11-21 11:48:39 +0100

    Update consultar_catalogo_de_libros_disponibles.php
    editar consultas

--- MS_HA/consultar_catalogo_de_libros_disponibles.php
+++ MS_HA/consultar_catalogo_de_libros_disponibles.php
@@ -1,42 +1,9 @@
-<?php
-require_once("conecta.php");
-
-// Verificar si se ha enviado el formulario
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Procesar el formulario cuando se envíe
-    $id_lector_consulta = $_POST['id_lector_consulta'];
-
-    // Crear la consulta SQL
-    $sql_prestamos_lector = "SELECT libros.nombre FROM prestamo JOIN libros ON prestamo.id_libro = libros.id WHERE prestamo.id_lector = $id_lector_consulta";
-
-    // Ejecutar la consulta
-    $resultado_prestamos_lector = mysqli_query($conexion, $sql_prestamos_lector);
-
-    // Verificar si el lector tiene ejemplares en el momento de la consulta
-    if ($resultado_prestamos_lector) {
-        echo "<h1>Préstamos realizados por el lector</h1>";
-
-        // Iterar sobre los resultados y mostrar los préstamos
-        while ($prestamo = mysqli_fetch_assoc($resultado_prestamos_lector)) {
-            echo "<p>Libro prestado: {$prestamo['nombre']}</p>";
-        }
-    } else {
-        echo "<p>Error al consultar préstamos: " . mysqli_error($conexion) . "</p>";
-    }
-
-    // Cerrar la conexión
-    mysqli_close($conexion);
-}
-?>
-
 <!DOCTYPE html>
 <html lang="es">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Consultar préstamos realizados por un lector</title>
-    <!-- Implantancion de los stilos css a la pagina -->
+    <title>Consultar catálogo de libros disponibles</title>
     <style>
         body {
             font-family: Arial, sans-serif;
@@ -52,31 +19,24 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             padding: 10px;
         }
 
-        form {
-            max-width: 400px;
+        table {
+            width: 80%;
             margin: 20px auto;
-            background-color: white;
-            padding: 20px;
-            border-radius: 8px;
-            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+            border-collapse: collapse;
         }
 
-        label {
-            display: block;
-            margin-bottom: 8px;
+        table, th, td {
+            border: 1px solid #ddd;
         }
 
-        input {
-            width: 100%;
-            padding: 8px;
-            margin-bottom: 16px;
-            box-sizing: border-box;
+        th, td {
+            padding: 10px;
+            text-align: left;
         }
 
-        input[type="submit"] {
+        th {
             background-color: #333;
             color: white;
-            cursor: pointer;
         }
 
         footer {
@@ -87,22 +47,47 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         }
     </style>
 </head>
-
 <body>
-    <header>
-        <h1>Consultar préstamos realizados por un lector</h1>
-    </header>
-    <!-- Creación del formulario -->
-    <form action="" method="post">
-        <label for="id_lector_consulta">ID del lector:</label>
-        <input type="text" name="id_lector_consulta" required>
+    <?php
+    require_once("conecta.php");
 
-        <input type="submit" value="Consultar préstamos">
-    </form>
+    
+    $sql_catalogo_libros = "SELECT nombre, n_disponibles, n_totales FROM libros";
+    
+    
+    $resultado_catalogo_libros = mysqli_query($conexion, $sql_catalogo_libros);
 
-    <footer>
-        <p>Integrantes del grupo: Hugo Antón, Miguel Sanz</p>
-    </footer>
+    
+    if ($resultado_catalogo_libros) {
+        echo "<header><h1>Consultar catálogo de libros disponibles</h1></header>";
+
+        
+        if (mysqli_num_rows($resultado_catalogo_libros) > 0) {
+            
+            echo "<table border='1'>";
+            echo "<tr><th>Nombre</th><th>Disponibles</th><th>Total</th></tr>";
+
+            
+            while ($libro = mysqli_fetch_assoc($resultado_catalogo_libros)) {
+                echo "<tr>";
+                echo "<td>{$libro['nombre']}</td>";
+                echo "<td>{$libro['n_disponibles']}</td>";
+                echo "<td>{$libro['n_totales']}</td>";
+                echo "</tr>";
+            }
+
+            echo "</table>";
+        } else {
+            echo "<p>No hay libros disponibles en este momento.</p>";
+        }
+
+        
+        mysqli_close($conexion);
+
+        echo "<footer><p>Integrantes del grupo: Hugo Antón, Miguel Sanz</p></footer>";
+    } else {
+        echo "<p>Error al consultar el catálogo de libros: " . mysqli_error($conexion) . "</p>";
+    }
+    ?>
 </body>
-
 </html>
