f099d411e54c567c3201999b8cc76ce34a77f30a
Hugo Antón Cuadrado <Hugoacacademico@gmail.com>
2023-11-22 08:43:27 +0100

    Revert "Dar de baja arreglado"
    This reverts commit fdc24db806dabd681deeef813107ab80ee3c80a7.

--- MS_HA/dar_de_baja_un_lector.php
+++ MS_HA/dar_de_baja_un_lector.php
@@ -9,11 +9,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $sql_verificar_prestamos = "SELECT n_prestado FROM lectores WHERE id = $id_lector_baja";
     $resultado_verificacion_prestamos = mysqli_query($conexion, $sql_verificar_prestamos);
     $n_prestado_lector = mysqli_fetch_assoc($resultado_verificacion_prestamos)['n_prestado'];
-    echo $n_prestado_lector;
 
-    if ($n_prestado_lector == 0) {
+    if ($n_prestado_lector === 0) {
         // Dar de baja al lector
-        mysqli_query($conexion, "UPDATE lectores SET estado = 'baja' WHERE id = $id_lector_baja");
+        mysqli_query($conexion, "DELETE lectores SET estado = 'baja' WHERE id = $id_lector_baja");
 
         echo "Lector dado de baja con éxito";
     } else {
